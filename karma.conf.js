75eaeb7a2fd3c3499d7e0fe9f7c545f81384fe48
Dan Stocker <dan@kwaia.com>
2015-09-26 21:10:39 +0100

    removes remaining references to namespace module from test configs

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,6 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'node_modules/giant-namespace/lib/giant-namespace.js',
             'node_modules/giant-assertion/lib/giant-assertion.js',
 
             'src/namespace.js',
