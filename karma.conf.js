01b5f731f21248fd36aa43e9ed0bcf7b365bd304
Dan Stocker <dan@kwaia.com>
2016-09-23 22:33:12 +0100

    upgrades to PhantomJs 2.1

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,6 @@ module.exports = function (config) {
 
         // list of files / patterns to load in the browser
         files: [
-            'node_modules/phantomjs-function-bind-polyfill/index.js',
-
             'node_modules/giant-assertion/lib/giant-assertion.js',
 
             'src/namespace.js',
